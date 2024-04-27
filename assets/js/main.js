4d2dbccee5182ef4e41e0dc41c81379277402d37
ajoy45 <ajoypust@gmail.com>
2024-04-28 00:31:42 +0600

    testimonial section done

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+    var swiper = new Swiper(".tp-testimonial-active", {
+        navigation: {
+            nextEl: ".tp-testimonial-slider-right",
+            prevEl: ".tp-testimonial-slider-left",
+        },
+
+    });
+
+})(jQuery)
