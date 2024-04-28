24b42af1e4b7a0cfb806e08261732f9fafbf8b06
ajoy45 <ajoypust@gmail.com>
2024-04-28 21:06:51 +0600

    team section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,38 @@
 (function ($) {
     "use strict";
-
+    // testimonial slider
     var swiper = new Swiper(".tp-testimonial-active", {
+        slidesPerView: 1,
+        loop: true,
         navigation: {
             nextEl: ".tp-testimonial-slider-right",
             prevEl: ".tp-testimonial-slider-left",
+        },
+
+    });
+
+    // brand slider
+    var swiper = new Swiper(".tp-brand-top-active", {
+        slidesPerView: "auto",
+        spaceBetween: 150,
+        loop: true,
+        // freemode: true,
+        speed: 2000,
+        centeredSlides: true,
+        allowTouchMove: false,
+
+        autoplay: {
+            delay: 1,
+            disableOnInteraction: true,
+        },
+    });
+    // Team slider
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        slidesPerView: 4,
+        navigation: {
+            nextEl: ".tp-team-swiper-next",
+            prevEl: ".tp-team-swiper-prev",
         },
 
     });
