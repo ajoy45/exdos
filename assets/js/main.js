7061b9bd2b680204f17e776f35b7bc2eb89047a1
ajoy45 <ajoypust@gmail.com>
2024-06-24 17:19:00 +0600

    mobile menu responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,32 @@
 (function ($) {
     "use strict";
+    // mobile menu 
+    var tpMobileMenu = $('.tp-mobile-menu-active > ul').clone();
+    var tpSideMenu = $('.tp-offcanvas-menu nav');
+    tpSideMenu.append(tpMobileMenu);
+    if ($(tpSideMenu).find('.sub-menu, .tp-mega-menu').length != 0) {
+        $(tpSideMenu).find('.sub-menu, .tp-mega-menu').parent().append('<button class="tp-menu-close"><i class="fas fa-chevron-right"></i></button>');
+    }
 
+    var sideMenuList = $('.tp-offcanvas-menu nav > ul > li button.tp-menu-close, .tp-offcanvas-menu nav > ul li.has-dropdown > a');
+    $(sideMenuList).on('click', function (e) {
+        console.log(e);
+        e.preventDefault();
+        if (!($(this).parent().hasClass('active'))) {
+            $(this).parent().addClass('active');
+            $(this).siblings('.sub-menu, .tp-mega-menu').slideDown();
+        } else {
+            $(this).siblings('.sub-menu, .tp-mega-menu').slideUp();
+            $(this).parent().removeClass('active');
+        }
+    });
+    // offcanvas
     $(".tp-header-offcanvas").on("click", function () {
-        $(".tp-offcanvas-wrapper").addClass("tp-offcanvas-open");
+        $(".tp-offcanvas").addClass("tp-offcanvas-open");
         $(".tp-all-overlay").addClass("tp-all-overlay-open");
     });
     $(".tp-offcanvas-close,.tp-all-overlay").on("click", function () {
-        $(".tp-offcanvas-wrapper").removeClass("tp-offcanvas-open");
+        $(".tp-offcanvas").removeClass("tp-offcanvas-open");
         $(".tp-all-overlay").removeClass("tp-all-overlay-open");
     });
 
