4a7ddef8327b781525c06bdb1bfd3b3bd1c12266
ajoy45 <ajoypust@gmail.com>
2024-08-19 13:01:36 +0600

    home-1 animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,4 +275,17 @@
 
     });
 
+    // wow activation
+    // wow = new WOW(
+    //     {
+    //         boxClass: 'wow',
+    //         animateClass: 'animated',
+    //         offset: 0,
+    //         mobile: true,
+    //         live: true
+    //     }
+    // )
+    // wow.init();
+    new WOW().init();
+
 })(jQuery)
